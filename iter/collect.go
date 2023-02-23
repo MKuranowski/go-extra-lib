7ecf670ffe1bfd84182f2768191c54b4f8e452ec
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-02-23 22:04:58 +0900

    Iter: collector functions

---
+++ iter/collect.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2023 Mikołaj Kuranowski
+// SPDX-License-Identifier: MIT
+
+package iter
+
+// IntoSlice collects all elements from an iterator into a single slice.
+func IntoSlice[T any](i Iterator[T]) []T {
+	s := make([]T, 0)
+	for i.Next() {
+		s = append(s, i.Get())
+	}
+	return s
+}
+
+// IntoMap collects all elements from an iterator into a map.
+func IntoMap[K comparable, V any](i Iterator[Pair[K, V]]) map[K]V {
+	m := make(map[K]V)
+	for i.Next() {
+		elem := i.Get()
+		m[elem.First] = elem.Second
+	}
+	return m
+}
+
+// IntoChannel spawns a new goroutine which sends all elements
+// from an iterator over a returned channel.
+//
+// After the iterator is exhausted the returned channel is closed.
+func IntoChannel[T any](i Iterator[T]) <-chan T {
+	ch := make(chan T)
+	go func() {
+		defer close(ch)
+		for i.Next() {
+			ch <- i.Get()
+		}
+	}()
+	return ch
+}
+
+// SendOver sends all elements from an iterator over a provided channel.
+// Blocks until the iterator is exhausted.
+// The provided channel is *not* closed.
+func SendOver[T any](i Iterator[T], out chan<- T) {
+	for i.Next() {
+		out <- i.Get()
+	}
+}
---
+++ iter/collect_test.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2023 Mikołaj Kuranowski
+// SPDX-License-Identifier: MIT
+
+package iter_test
+
+import (
+	"testing"
+
+	. "github.com/MKuranowski/go-extra-lib/iter"
+	"github.com/MKuranowski/go-extra-lib/testing2/assert"
+)
+
+func TestIntoSlice(t *testing.T) {
+	expected := []int{1, 2, 3}
+	i := OverSlice(expected)
+	got := IntoSlice(i)
+
+	assert.DeepEq(t, got, expected)
+	assert.NoErrMsg(t, i.Err(), "i.Err()")
+}
+
+func TestIntoMap(t *testing.T) {
+	expected := map[int]string{1: "1", 2: "2", 3: "3"}
+	i := OverMap(expected)
+	got := IntoMap(i)
+
+	assert.DeepEq(t, got, expected)
+	assert.NoErrMsg(t, i.Err(), "i.Err()")
+}
+
+func TestIntoChannel(t *testing.T) {
+	i := OverSlice([]int{1, 2, 3})
+	ch := IntoChannel(i)
+
+	assert.EqMsg(t, <-ch, 1, "1st element")
+	assert.EqMsg(t, <-ch, 2, "2nd element")
+	assert.EqMsg(t, <-ch, 3, "3rd element")
+
+	_, isOpen := <-ch
+	assert.FalseMsg(t, isOpen, "channel closed")
+
+	assert.NoErrMsg(t, i.Err(), "i.Err()")
+}
+
+func TestSendOver(t *testing.T) {
+	i := OverSlice([]int{1, 2, 3})
+	ch := make(chan int)
+	go SendOver(i, ch)
+
+	assert.EqMsg(t, <-ch, 1, "1st element")
+	assert.EqMsg(t, <-ch, 2, "2nd element")
+	assert.EqMsg(t, <-ch, 3, "3rd element")
+	assert.NoErrMsg(t, i.Err(), "i.Err()")
+}
