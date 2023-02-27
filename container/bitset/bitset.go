464873ef95ebb4cfd913b65ef3f9aa6ce190583c
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-02-27 23:53:58 +0900

    Bitset container

---
+++ container/bitset/bitset.go
@@ -0,0 +1,231 @@
+// Copyright (c) 2023 Mikołaj Kuranowski
+// SPDX-License-Identifier: MIT
+
+// bitset contains an efficient implementation of a set of unsigned numbers.
+package bitset
+
+import (
+	"math/big"
+	"math/bits"
+
+	"github.com/MKuranowski/go-extra-lib/iter"
+)
+
+var (
+	bigZero = big.Int{}
+)
+
+// BitSet is a set of (almost) arbitrary-sized integers.
+//
+// The zero value (`&BitSet{}`) is a BitSet containing no elements.
+//
+// The representation uses [big.Int] to check whether a number is included in the set,
+// so a map-based set may be a better use-case for sparse sets without any upper-bound.
+//
+// Even tough most operations accept `int` as an argument,
+// those functions will panic if the provided number is negative.
+//
+// See also [Small] - a more efficient implementation if elements are known
+// to be in range [0, 63] inclusive - and which can be used as a key in a map
+// (by fulfilling the comparable protocol).
+type BitSet struct {
+	n big.Int
+}
+
+// Of returns a BitSet containing all the provided elements
+func Of(is ...int) *BitSet {
+	b := &BitSet{}
+	for _, i := range is {
+		b.Add(i)
+	}
+	return b
+}
+
+// Has returns true if the provided number is in the set.
+func (s *BitSet) Has(i int) bool { return s.n.Bit(i) != 0 }
+
+// Add ensures that the provided number is in the set.
+func (s *BitSet) Add(i int) { s.n.SetBit(&s.n, i, 1) }
+
+// Remove ensures that the provided number is not in the set.
+func (s *BitSet) Remove(i int) { s.n.SetBit(&s.n, i, 0) }
+
+// Len returns the number of elements in the set.
+func (s *BitSet) Len() int {
+	n := 0
+	for _, word := range s.n.Bits() {
+		n += bits.OnesCount(uint(word))
+	}
+	return n
+}
+
+// Clear ensures that no numbers are present in the set.
+func (s *BitSet) Clear() { s.n.SetUint64(0) }
+
+// Clone returns a new set with the same elements.
+func (s *BitSet) Clone() *BitSet {
+	n := &BitSet{}
+	n.n.Set(&s.n)
+	return n
+}
+
+// Equal returns true if s1 contains the same elements as s2.
+func (s1 *BitSet) Equal(s2 *BitSet) bool { return s1.n.Cmp(&s2.n) == 0 }
+
+// Union ensures s1 contains all elements from s2.
+func (s1 *BitSet) Union(s2 *BitSet) { s1.n.Or(&s1.n, &s2.n) }
+
+// Intersection ensures s1 only contains elements that are present in both s1 and s2.
+func (s1 *BitSet) Intersection(s2 *BitSet) { s1.n.And(&s1.n, &s2.n) }
+
+// Difference ensures s1 does not contain any elements from s1.
+func (s1 *BitSet) Difference(s2 *BitSet) { s1.n.AndNot(&s1.n, &s2.n) }
+
+// IsDisjoined returns true if s1 and s2 have no elements in common.
+func (s1 *BitSet) IsDisjoined(s2 *BitSet) bool {
+	return (&big.Int{}).And(&s1.n, &s2.n).Cmp(&bigZero) == 0
+}
+
+// IsSubset returns true if every element of s1 is also present in s2.
+func (s1 *BitSet) IsSubset(s2 *BitSet) bool {
+	return (&big.Int{}).And(&s1.n, &s2.n).Cmp(&s1.n) == 0
+}
+
+// IsSuperset returns true if every element of s2 is also present in s1.
+func (s1 *BitSet) IsSuperset(s2 *BitSet) bool {
+	return (&big.Int{}).And(&s2.n, &s1.n).Cmp(&s2.n) == 0
+}
+
+type bitsetIterator struct {
+	s       BitSet
+	n       int
+	started bool
+}
+
+func (i *bitsetIterator) Next() bool {
+	// Shift out the last-generated element, except if there was no such element
+	if i.started {
+		i.s.n.Rsh(&i.s.n, 1)
+		i.n++
+	} else {
+		i.started = true
+	}
+
+	if i.s.n.Cmp(&bigZero) == 0 {
+		return false
+	}
+
+	// Calculate the offset to the next number
+	offset := i.s.n.TrailingZeroBits()
+	i.s.n.Rsh(&i.s.n, offset)
+	i.n += int(offset)
+
+	if i.s.n.Bit(0) == 0 {
+		panic("big.Int.TrailingZeroBits() has lied")
+	}
+	return true
+}
+
+func (i bitsetIterator) Get() int { return i.n }
+func (bitsetIterator) Err() error { return nil }
+
+func (s *BitSet) Iter() iter.Iterator[int] {
+	return &bitsetIterator{s: *s}
+}
+
+// Small is a set of integers between 0 and 63 (inclusive),
+// which also fulfills the `comparable` interface and can be used e.g. as a map key.
+//
+// The zero value (`Small(0)`) is a set containing no elements.
+//
+// The representation is a simple wrapper around uint64.
+//
+// Trying to provide elements outside of the <0, 63> range is undefined behavior.
+//
+// See also [BitSet] - an implementation for (almost) arbitrary sized elements.
+type Small uint64
+
+// SmallOf returns a bitset containing the provided numbers.
+func SmallOf(is ...int) Small {
+	s := Small(0)
+	for _, i := range is {
+		s.Add(i)
+	}
+	return s
+}
+
+// Has returns true if the provided number is in the set.
+func (s Small) Has(i int) bool { return (s>>Small(i))&1 != 0 }
+
+// Add ensures that the provided number is in the set.
+func (s *Small) Add(i int) { *s |= 1 << Small(i) }
+
+// Remove ensures that the provided number is not in the set.
+func (s *Small) Remove(i int) { *s &^= 1 << Small(i) }
+
+// Len returns the number of elements in the set.
+func (s Small) Len() int { return bits.OnesCount64(uint64(s)) }
+
+// Clear ensures that no numbers are present in the set.
+func (s *Small) Clear() { *s = 0 }
+
+// Clone returns a new set with the same elements.
+func (s Small) Clone() Small { return s }
+
+// Equal returns true if b1 contains the same elements as b2.
+func (s1 Small) Equal(s2 Small) bool { return s1 == s2 }
+
+// Union ensures b1 contains all elements from b2.
+func (s1 *Small) Union(s2 Small) { *s1 |= s2 }
+
+// Intersection ensures b1 only contains elements that are present in both b1 and b2.
+func (s1 *Small) Intersection(s2 Small) { *s1 &= s2 }
+
+// Difference ensures b1 does not contain any elements from b2.
+func (s1 *Small) Difference(s2 Small) { *s1 &^= s2 }
+
+// IsDisjoined returns true if s1 and s2 have no elements in common.
+func (s1 Small) IsDisjoined(s2 Small) bool { return s1&s2 == 0 }
+
+// IsSubset returns true if every element of s1 is also present in s2.
+func (s1 Small) IsSubset(s2 Small) bool { return s1&s2 == s1 }
+
+// IsSuperset returns true if every element of s2 is also present in s1.
+func (s1 Small) IsSuperset(s2 Small) bool { return s2&s1 == s2 }
+
+type smallIterator struct {
+	s       uint64
+	n       int
+	started bool
+}
+
+func (i *smallIterator) Next() bool {
+	// Shift out the last-generated element, except if there was no such element
+	if i.started {
+		i.s, i.n = i.s>>1, i.n+1
+	} else {
+		i.started = true
+	}
+
+	if i.s == 0 {
+		return false
+	}
+
+	// Calculate the offset to the next number
+	offset := bits.TrailingZeros64(i.s)
+	i.s, i.n = i.s>>offset, i.n+offset
+
+	if i.s&1 == 0 {
+		panic("TrailingZeroBits64 has lied")
+	}
+	return true
+}
+
+func (i smallIterator) Get() int { return i.n }
+func (smallIterator) Err() error { return nil }
+
+// Iter returns an iterator over the elements in the set.
+//
+// Any changes made during iteration are not reflected in the iterator;
+// iteration is actually performed on a copy of the set.
+func (s Small) Iter() iter.Iterator[int] { return &smallIterator{s: uint64(s)} }
---
+++ container/bitset/bitset_test.go
@@ -0,0 +1,421 @@
+// Copyright (c) 2023 Mikołaj Kuranowski
+// SPDX-License-Identifier: MIT
+
+package bitset_test
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/MKuranowski/go-extra-lib/container/bitset"
+	"github.com/MKuranowski/go-extra-lib/iter"
+	"github.com/MKuranowski/go-extra-lib/testing2/check"
+)
+
+// BitSet
+
+func TestBitSetAddHasLenRemove(t *testing.T) {
+	s := &BitSet{}
+
+	check.EqMsg(t, s.Len(), 0, "s.Len(): empty set")
+
+	s.Add(2)
+	s.Add(3)
+	s.Add(5)
+	s.Add(7)
+
+	check.EqMsg(t, s.Len(), 4, "s.Len(): after adding")
+	check.FalseMsg(t, s.Has(0), "s.Has(0): after adding")
+	check.FalseMsg(t, s.Has(1), "s.Has(1): after adding")
+	check.TrueMsg(t, s.Has(2), "s.Has(2): after adding")
+	check.TrueMsg(t, s.Has(3), "s.Has(3): after adding")
+	check.FalseMsg(t, s.Has(4), "s.Has(4): after adding")
+	check.TrueMsg(t, s.Has(5), "s.Has(5): after adding")
+	check.FalseMsg(t, s.Has(6), "s.Has(6): after adding")
+	check.TrueMsg(t, s.Has(7), "s.Has(7): after adding")
+
+	s.Add(2)
+
+	check.EqMsg(t, s.Len(), 4, "s.Len(): after adding duplicate")
+	check.TrueMsg(t, s.Has(2), "s.Has(2): after adding duplicate")
+
+	s.Remove(5)
+	s.Remove(7)
+
+	check.EqMsg(t, s.Len(), 2, "s.Len(): after removing")
+	check.FalseMsg(t, s.Has(0), "s.Has(0): after removing")
+	check.FalseMsg(t, s.Has(1), "s.Has(1): after removing")
+	check.TrueMsg(t, s.Has(2), "s.Has(2): after removing")
+	check.TrueMsg(t, s.Has(3), "s.Has(3): after removing")
+	check.FalseMsg(t, s.Has(4), "s.Has(4): after removing")
+	check.FalseMsg(t, s.Has(5), "s.Has(5): after removing")
+	check.FalseMsg(t, s.Has(6), "s.Has(6): after removing")
+	check.FalseMsg(t, s.Has(7), "s.Has(7): after removing")
+}
+
+func TestBitSetOf(t *testing.T) {
+	s := Of(1, 3, 5)
+
+	check.EqMsg(t, s.Len(), 3, "s.Len()")
+	check.TrueMsg(t, s.Has(1), "s.Has(1)")
+	check.TrueMsg(t, s.Has(3), "s.Has(3)")
+	check.TrueMsg(t, s.Has(5), "s.Has(5)")
+}
+
+func TestBitSetClear(t *testing.T) {
+	s := &BitSet{}
+
+	s.Add(2)
+	s.Add(3)
+	s.Add(5)
+	s.Add(7)
+
+	check.EqMsg(t, s.Len(), 4, "s.Len(): after adding")
+
+	s.Clear()
+
+	check.EqMsg(t, s.Len(), 0, "s.Len(): after clearing")
+}
+
+func TestBitSetClone(t *testing.T) {
+	s1 := &BitSet{}
+
+	s1.Add(1)
+	s1.Add(3)
+
+	s2 := s1.Clone()
+
+	s2.Add(5)
+
+	check.EqMsg(t, s1.Len(), 2, "s1.Len()")
+	check.EqMsg(t, s2.Len(), 3, "s2.Len()")
+}
+
+func TestBitSetEqual(t *testing.T) {
+	s1, s2 := new(BitSet), new(BitSet)
+
+	s1.Add(2)
+	s1.Add(3)
+
+	check.FalseMsg(t, s1.Equal(s2), "{2, 3} == {}")
+
+	s2.Add(2)
+	s2.Add(3)
+
+	check.TrueMsg(t, s1.Equal(s2), "{2, 3} == {2, 3}")
+
+	s1.Remove(3)
+
+	check.FalseMsg(t, s1.Equal(s2), "{2} == {2, 3}")
+}
+
+func TestBitSetUnion(t *testing.T) {
+	s := Of(1, 3, 5)
+	check.EqMsg(t, s.Len(), 3, "s.Len(): before union")
+
+	s.Union(Of(2, 3, 4))
+	check.EqMsg(t, s.Len(), 5, "s.Len(): after union")
+
+	for i := 0; i <= 6; i++ {
+		if i >= 1 && i <= 5 {
+			check.TrueMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		} else {
+			check.FalseMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		}
+	}
+}
+
+func TestBitSetIntersection(t *testing.T) {
+	s := Of(1, 3, 5)
+	check.EqMsg(t, s.Len(), 3, "s.Len(): before intersection")
+
+	s.Difference(Of(3, 4, 5))
+	check.EqMsg(t, s.Len(), 1, "s.Len(): after intersection")
+
+	for i := 0; i <= 6; i++ {
+		if i == 1 {
+			check.TrueMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		} else {
+			check.FalseMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		}
+	}
+}
+
+func TestBitSetDifference(t *testing.T) {
+	s := Of(1, 3, 5)
+	check.EqMsg(t, s.Len(), 3, "s.Len(): before difference")
+
+	s.Difference(Of(2, 3, 4))
+	check.EqMsg(t, s.Len(), 2, "s.Len(): after difference")
+
+	for i := 0; i <= 6; i++ {
+		if i == 1 || i == 5 {
+			check.TrueMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		} else {
+			check.FalseMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		}
+	}
+}
+
+func TestBitSetIter(t *testing.T) {
+	check.DeepEqMsg(
+		t,
+		iter.IntoSlice(Of(1, 3, 11, 128, 1024).Iter()),
+		[]int{1, 3, 11, 128, 1024},
+		"Of(1, 3, 11, 128, 1024).Iter()",
+	)
+
+	check.DeepEqMsg(t, iter.IntoSlice((&BitSet{}).Iter()), []int{}, "Of().Iter()")
+}
+
+// Small
+
+func TestSmallAddHasLenRemove(t *testing.T) {
+	s := Small(0)
+
+	check.EqMsg(t, s.Len(), 0, "s.Len(): empty set")
+
+	s.Add(2)
+	s.Add(3)
+	s.Add(5)
+	s.Add(7)
+
+	check.EqMsg(t, s.Len(), 4, "s.Len(): after adding")
+	check.FalseMsg(t, s.Has(0), "s.Has(0): after adding")
+	check.FalseMsg(t, s.Has(1), "s.Has(1): after adding")
+	check.TrueMsg(t, s.Has(2), "s.Has(2): after adding")
+	check.TrueMsg(t, s.Has(3), "s.Has(3): after adding")
+	check.FalseMsg(t, s.Has(4), "s.Has(4): after adding")
+	check.TrueMsg(t, s.Has(5), "s.Has(5): after adding")
+	check.FalseMsg(t, s.Has(6), "s.Has(6): after adding")
+	check.TrueMsg(t, s.Has(7), "s.Has(7): after adding")
+
+	s.Add(2)
+
+	check.EqMsg(t, s.Len(), 4, "s.Len(): after adding duplicate")
+	check.TrueMsg(t, s.Has(2), "s.Has(2): after adding duplicate")
+
+	s.Remove(5)
+	s.Remove(7)
+
+	check.EqMsg(t, s.Len(), 2, "s.Len(): after removing")
+	check.FalseMsg(t, s.Has(0), "s.Has(0): after removing")
+	check.FalseMsg(t, s.Has(1), "s.Has(1): after removing")
+	check.TrueMsg(t, s.Has(2), "s.Has(2): after removing")
+	check.TrueMsg(t, s.Has(3), "s.Has(3): after removing")
+	check.FalseMsg(t, s.Has(4), "s.Has(4): after removing")
+	check.FalseMsg(t, s.Has(5), "s.Has(5): after removing")
+	check.FalseMsg(t, s.Has(6), "s.Has(6): after removing")
+	check.FalseMsg(t, s.Has(7), "s.Has(7): after removing")
+}
+
+func TestSmallOf(t *testing.T) {
+	s := SmallOf(1, 3, 5)
+
+	check.EqMsg(t, s.Len(), 3, "s.Len()")
+	check.TrueMsg(t, s.Has(1), "s.Has(1)")
+	check.TrueMsg(t, s.Has(3), "s.Has(3)")
+	check.TrueMsg(t, s.Has(5), "s.Has(5)")
+}
+
+func TestSmallClear(t *testing.T) {
+	s := Small(0)
+
+	s.Add(2)
+	s.Add(3)
+	s.Add(5)
+	s.Add(7)
+
+	check.EqMsg(t, s.Len(), 4, "s.Len(): after adding")
+
+	s.Clear()
+
+	check.EqMsg(t, s.Len(), 0, "s.Len(): after clearing")
+}
+
+func TestSmallClone(t *testing.T) {
+	s1 := Small(0)
+
+	s1.Add(1)
+	s1.Add(3)
+
+	s2 := s1.Clone()
+
+	s2.Add(5)
+
+	check.EqMsg(t, s1.Len(), 2, "s1.Len()")
+	check.EqMsg(t, s2.Len(), 3, "s2.Len()")
+}
+
+func TestSmallEqual(t *testing.T) {
+	s1, s2 := new(Small), new(Small)
+
+	s1.Add(2)
+	s1.Add(3)
+
+	check.FalseMsg(t, s1.Equal(*s2), "{2, 3} == {}")
+
+	s2.Add(2)
+	s2.Add(3)
+
+	check.TrueMsg(t, s1.Equal(*s2), "{2, 3} == {2, 3}")
+
+	s1.Remove(3)
+
+	check.FalseMsg(t, s1.Equal(*s2), "{2} == {2, 3}")
+}
+
+func TestSmallUnion(t *testing.T) {
+	s := SmallOf(1, 3, 5)
+	check.EqMsg(t, s.Len(), 3, "s.Len(): before union")
+
+	s.Union(SmallOf(2, 3, 4))
+	check.EqMsg(t, s.Len(), 5, "s.Len(): after union")
+
+	for i := 0; i <= 6; i++ {
+		if i >= 1 && i <= 5 {
+			check.TrueMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		} else {
+			check.FalseMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		}
+	}
+}
+
+func TestSmallIntersection(t *testing.T) {
+	s := SmallOf(1, 3, 5)
+	check.EqMsg(t, s.Len(), 3, "s.Len(): before intersection")
+
+	s.Difference(SmallOf(3, 4, 5))
+	check.EqMsg(t, s.Len(), 1, "s.Len(): after intersection")
+
+	for i := 0; i <= 6; i++ {
+		if i == 1 {
+			check.TrueMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		} else {
+			check.FalseMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		}
+	}
+}
+
+func TestSmallDifference(t *testing.T) {
+	s := SmallOf(1, 3, 5)
+	check.EqMsg(t, s.Len(), 3, "s.Len(): before difference")
+
+	s.Difference(SmallOf(2, 3, 4))
+	check.EqMsg(t, s.Len(), 2, "s.Len(): after difference")
+
+	for i := 0; i <= 6; i++ {
+		if i == 1 || i == 5 {
+			check.TrueMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		} else {
+			check.FalseMsg(t, s.Has(i), fmt.Sprintf("s.Has(%d)", i))
+		}
+	}
+}
+
+func TestSmallIsDisjoined(t *testing.T) {
+	check.FalseMsg(
+		t,
+		SmallOf(1, 2, 3).IsDisjoined(SmallOf(1, 2, 3)),
+		"SmallOf(1, 2, 3).IsDisjoined(SmallOf(1, 2, 3))",
+	)
+
+	check.TrueMsg(
+		t,
+		SmallOf(1, 2, 3).IsDisjoined(SmallOf(4, 5, 6)),
+		"SmallOf(1, 2, 3).IsDisjoined(SmallOf(4, 5, 6))",
+	)
+
+	check.FalseMsg(
+		t,
+		SmallOf(1, 2, 3).IsDisjoined(SmallOf(3, 4, 5)),
+		"SmallOf(1, 2, 3).IsDisjoined(SmallOf(3, 5, 4))",
+	)
+}
+
+func TestSmallIsSubset(t *testing.T) {
+	check.TrueMsg(
+		t,
+		SmallOf(1, 2, 3).IsSubset(SmallOf(1, 2, 3)),
+		"SmallOf(1, 2, 3).IsSubset(SmallOf(1, 2, 3))",
+	)
+
+	check.TrueMsg(
+		t,
+		SmallOf(1, 2).IsSubset(SmallOf(1, 2, 3)),
+		"SmallOf(1, 2).IsSubset(SmallOf(1, 2, 3))",
+	)
+
+	check.FalseMsg(
+		t,
+		SmallOf(1, 2, 3).IsSubset(SmallOf(1, 2)),
+		"SmallOf(1, 2, 3).IsSubset(SmallOf(1, 2))",
+	)
+
+	check.FalseMsg(
+		t,
+		SmallOf(1, 2, 3).IsSubset(SmallOf(1, 2, 4, 5)),
+		"SmallOf(1, 2, 3).IsSubset(SmallOf(1, 2, 4, 5))",
+	)
+
+	check.TrueMsg(
+		t,
+		SmallOf().IsSubset(SmallOf()),
+		"SmallOf().IsSubset(SmallOf())",
+	)
+
+	check.TrueMsg(
+		t,
+		SmallOf().IsSubset(SmallOf(1, 2, 3)),
+		"SmallOf().IsSubset(SmallOf(1, 2, 3))",
+	)
+}
+
+func TestSmallIsSuperset(t *testing.T) {
+	check.TrueMsg(
+		t,
+		SmallOf(1, 2, 3).IsSuperset(SmallOf(1, 2, 3)),
+		"SmallOf(1, 2, 3).IsSuperset(SmallOf(1, 2, 3))",
+	)
+
+	check.FalseMsg(
+		t,
+		SmallOf(1, 2).IsSuperset(SmallOf(1, 2, 3)),
+		"SmallOf(1, 2).IsSuperset(SmallOf(1, 2, 3))",
+	)
+
+	check.TrueMsg(
+		t,
+		SmallOf(1, 2, 3).IsSuperset(SmallOf(1, 2)),
+		"SmallOf(1, 2, 3).IsSuperset(SmallOf(1, 2))",
+	)
+
+	check.FalseMsg(
+		t,
+		SmallOf(1, 2, 3).IsSuperset(SmallOf(1, 2, 4, 5)),
+		"SmallOf(1, 2, 3).IsSuperset(SmallOf(1, 2, 4, 5))",
+	)
+
+	check.TrueMsg(
+		t,
+		SmallOf().IsSuperset(SmallOf()),
+		"SmallOf().IsSuperset(SmallOf())",
+	)
+
+	check.FalseMsg(
+		t,
+		SmallOf().IsSuperset(SmallOf(1, 2, 3)),
+		"SmallOf().IsSuperset(SmallOf(1, 2, 3))",
+	)
+}
+
+func TestSmallIter(t *testing.T) {
+	check.DeepEqMsg(
+		t,
+		iter.IntoSlice(SmallOf(1, 3, 11, 60).Iter()),
+		[]int{1, 3, 11, 60},
+		"SmallOf(1, 3, 11, 60).Iter()",
+	)
+
+	check.DeepEqMsg(t, iter.IntoSlice(Small(0).Iter()), []int{}, "Of().Iter()")
+}
